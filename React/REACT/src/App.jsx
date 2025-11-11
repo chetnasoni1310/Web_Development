dfea5612966c8d018447d1b3bd0e6bb42befdc84
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-11-11 15:29:09 +0530

    'learned how to create a component'

--- React/REACT/src/App.jsx
+++ React/REACT/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Pehle toh react mai bna rhe hai toh react ko import krlenge
+//Fir humein component bnana hai 
+//component is something structural jismai data nahi bharte hai bss bta dete hai ki data kaha aayega dhaancha taiyrrat kr dete hai filling nhi krte hai 
+//Ismai jo file ka naam hai vhi component kaa naam bhi hota hai 
+//And anything or any function jo createElement / jsx ko return kre voh component hota hai 
+//Jo function jsx ko return krta hai usko functional component kehte hai 
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// import React from "react"; <-- still works fine in newer verisons
+
+export function App(){
+return (
+<> <h1>Hey</h1>
+  <h1>Hi</h1>
+  <h1>Yoyo</h1>
+  <h1>yahoo</h1>
+</>
+)
 }
 
-export default App
+
+export function App2(){
+return (
+ <div>
+   <h1>Hey</h1>
+    <h1>Hi</h1>
+    <h1>Yoyo</h1>
+    <h1>yahoo</h1>
+ </div>
+)
+}
+ 
--- React/REACT/src/main.jsx
+++ React/REACT/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+// import App2 from './App.jsx' <-- Yeh wale tab use karte hai jab hum export default app use krte 
+import { App, App2 } from './App.jsx'    // <--Yeh tab use krte hai jab export function app(){} use krte hai 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
