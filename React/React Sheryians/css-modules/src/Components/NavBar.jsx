9a74d3af493412988d8a60384357251b2c922b52
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-11-19 15:48:15 +0530

    'Learn how to use CSS modules in react'

---
+++ React/React Sheryians/css-modules/src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from "./style.module.css";
+
+function NavBar() {
+  return (
+    <div>
+      <h1 className={style.a}> Orange</h1>
+      <h1 className={style.b}> Orange</h1>
+      <h1 className={style.a}> Orange</h1>
+    </div>
+  )
+}
+
+export default NavBar
+
