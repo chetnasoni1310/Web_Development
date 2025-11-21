31dbc7f2c4bfb8fc743be0f14bf6a9f85803a652
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-11-21 14:54:14 +0530

    'small ui updation'

--- React/react-routing/api-basics/src/Components/Products.jsx
+++ React/react-routing/api-basics/src/Components/Products.jsx
@@ -11,7 +11,7 @@ function Products({productData}) {
     <div className='flex flex-wrap gap-10 justify-center align-center'>
       {
         productData.map((item , index)=>{
-          return <div key={index} className='bg-zinc-200 w-[30rem] h-[35rem] rounded-4xl overflow-hidden' >
+          return <div key={index} className='bg-zinc-200 w-120 h-140 rounded-4xl overflow-hidden' >
               <img src={item.image} alt="" className='w-full h-[60%] object-cover'/>
               <h1 className='px-10 text-4xl font-semibold mt-4'>{item.title}</h1>
               <h2 className='px-10 text-xs pt-2 font-semibold  mt-2'>{item.description}</h2>
