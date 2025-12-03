1ec157c45347eee0b28aabe9bd7f3571d07dde1a
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-12-03 12:14:14 +0530

    'Implemented scrolling control over content'

---
+++ Developer-Problems/Scrolling-Control/script.js
@@ -0,0 +1,6 @@
+const scrollButton = document.querySelector('.scroll');
+
+scrollButton.addEventListener('click' , ()=>{
+    // document.body.classList.add('overflow-hidden');
+    document.body.classList.toggle('overflow-hidden');
+})
